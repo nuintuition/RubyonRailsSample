373dd1a3b47790a0b353ad125ae0f3b5c442d9e1
harry <harry@harrydeiMac.local>
2019-05-02 22:31:00 +0800

    add new view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+    def index
+        end
+    def first
+        end
+    def second
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
+  #to is key "home#first" is value
+  # like .js get(v1,v2)
+  get "first", to:"home#first"
+  get "second", to:"home#second"
 end
