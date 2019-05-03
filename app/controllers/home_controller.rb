a7c1402b433888fc3dda7c04022317aeb2795be4
harry <harry@harrydeiMac.local>
2019-05-03 18:13:14 +0800

    add partial partial找對應的layouts時會在當前目錄先找當前目錄在root中設為home 如果找不到再到同層的資料夾中找
    另外可以在controller中設定參數來控制這個layouts要不要顯示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
     def index
+        @nav_flag = false
         end
     def first
+        @nav_flag = true
         end
     def second
+        @nav_flag = false
         end
     def show_post
         #@全域變數 宣告相同名稱會前後bing起來 params預設用來接傳進來的參數
