3d4344dada0686900d05687171c2e814829f5ef0
harry <harry@harrydeiMac.local>
2019-05-06 19:17:58 +0800

    改用gem安裝jquery與bootstrap 如果要更新套件在cmd使用bundle update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require ./jquery-3.4.1.min
-//= require_tree ./bootstrap
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
