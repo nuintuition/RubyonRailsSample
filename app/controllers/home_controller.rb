255a979291b96d10e77267d76255511d4096a343
harry <harry@harrydeiMac.local>
2019-05-07 18:53:31 +0800

    用迴圈方式建立array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,31 +6,15 @@ class HomeController < ApplicationController
             action_title: "閱讀更多",
         }
 
-        @products =[
-            {
-                id:"1",
-                name: "商品1",
-                desription:"商品描述1",
-                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            },
-            {
-                id:"2",
-                name: "商品2",
-                desription:"商品描述2",
-                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            },
-            {
-                id:"3",
-                name: "商品3",
-                desription:"商品描述3",
-                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            },
-            {
-                id:"4",
-                name: "商品4",
-                desription:"商品描述4",
+        @products = []
+        (1..4).each do |index|
+            product = {
+                id:index,
+                name: "商品#{index}",
+                desription:"商品描述#{index}",
                 image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             }
-        ]
+            @products.push(product)
+        end
     end
 end
