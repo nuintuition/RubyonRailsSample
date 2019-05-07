dc0a856aef32f67728847a64bce1e89f8da06156
harry <harry@harrydeiMac.local>
2019-05-07 18:23:19 +0800

    使用foreach的方法建立html物件
    執行物件時加上{}會如同匿名方法依樣可在裡面做功能運算 a.each{ |b|  b+1 }

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,32 @@ class HomeController < ApplicationController
             des: "這是廣告",
             action_title: "閱讀更多",
         }
+
+        @products =[
+            {
+                id:"1",
+                name: "商品1",
+                desription:"商品描述1",
+                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            },
+            {
+                id:"2",
+                name: "商品2",
+                desription:"商品描述2",
+                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            },
+            {
+                id:"3",
+                name: "商品3",
+                desription:"商品描述3",
+                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            },
+            {
+                id:"4",
+                name: "商品4",
+                desription:"商品描述4",
+                image_url: "https://images.pexels.com/photos/2120483/pexels-photo-2120483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            }
+        ]
     end
 end
