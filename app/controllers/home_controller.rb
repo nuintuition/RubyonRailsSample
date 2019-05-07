8aa96567a0c5969588297cd53933ff156a599a73
harry <harry@harrydeiMac.local>
2019-05-07 17:48:53 +0800

    前端與後端文字連結並使用hash完成
    ruby on rails 所有變數都是物件
    :a != “a” :是symbol的意思 symbol是無法變更如果改變就是指向一個新的位址
    a = {} 是指 a是一個hash 內容是空的

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
     def index
-        @nav_flag = false
-        end
+        @ad ={
+            title: "大型廣告",
+            des: "這是廣告",
+            action_title: "閱讀更多",
+        }
+    end
 end
