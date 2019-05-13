699d29991113fcf8319547f115bb95b4e0444def
harry <harry@harrydeiMac.local>
2019-05-13 21:33:25 +0800

    新增一個categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_090114) do
+ActiveRecord::Schema.define(version: 2019_05_13_125438) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
